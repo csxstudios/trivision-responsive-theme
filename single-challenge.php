e21b99a335feff69c26349f1864d3c9bb743984e
csxstudios <csxstudios@gmail.com>
2017-04-26 16:39:09 -0400

    Add 30px padding above content loop

--- single-challenge.php
+++ single-challenge.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-			<div id="body" class="single">
+			<div id="body" class="single pad-30">
 			<?php while (have_posts()) : the_post()?>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
