ba64aa72969f8266135b55b3a729b58be9bacdaa
csxstudios <chris@csxstudios.com>
2017-06-20 10:10:30 -0400

    Added page templates with sidebars.

---
+++ page-2col-right-sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Template Name: 2 Column - Right Sidebar
+ * Description: Loop to show thumbnails of subpages of this page.
+ */
+get_header();
+?>
+			<div id="body<?php if (is_front_page()) {echo '-home';}?>">
+				<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+					<?php while (have_posts()) : the_post()?>
+						<?php the_content(); ?>
+					<?php endwhile; ?>
+					</div>
+					<div class="col-md-4">
+					<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar')) : ?>
+					<!-- no dynamic sidebar -->			
+					<?php endif; ?>
+					</div>					
+				</div>
+				</div>
+			</div>
+			<?php if ($post->post_name !== 'contact-us' and !is_front_page()) {?><div class="pad-0"></div><?php } ?>
+		</section>
+<?php get_footer(); ?>
